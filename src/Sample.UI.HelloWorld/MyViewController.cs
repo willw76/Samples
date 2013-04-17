62fa7b4e2796b7123cc824eeab0bf58247a28512
William Wang <will_w76@hotmail.com>
2013-04-17 23:06:43 +0800

    Presenter demonstration

--- src/Sample.UI.HelloWorld/MyViewController.cs
+++ src/Sample.UI.HelloWorld/MyViewController.cs
@@ -2,79 +2,92 @@ using System;
 using MonoTouch.UIKit;
 using System.Drawing;
 using Sample.UI.HelloWorld.BindingExtensions;
-using Sample.UI.Model.Implementation;
 using Sample.UI.Model.Interfaces;
 
 namespace Sample.UI.HelloWorld
 {
-    public class MyViewController : UIViewController
+    public class MyViewController : UIViewController, ISimpleView
     {
         private UIButton _buttonSetLabel;
         private UIButton _buttonSetText;
         private UILabel _label;
         private UITextField _textField;
-        int numClicks = 0;
-        float buttonWidth = 200;
-        float buttonHeight = 50;
+        //private int _numberOfClicks;
+        private const float _buttonWidth = 200;
+        private const float _buttonHeight = 50;
+        private ISimpleModel Model { get; set; }
+        public ISimplePresenter Presenter { get; set; }
+
+        public void InitView ()
+        {
+            Console.Out.WriteLine("InitView()");
+            _buttonSetLabel = UIButton.FromType(UIButtonType.RoundedRect);
+            _label = new UILabel();
+
+            _buttonSetText = UIButton.FromType(UIButtonType.RoundedRect);
+            _textField = new UITextField();
+        }
         
         public override void ViewDidLoad()
         {
+            Console.Out.WriteLine("ViewDidLoad()");
             base.ViewDidLoad();
 
             View.Frame = UIScreen.MainScreen.Bounds;
             View.BackgroundColor = UIColor.White;
             View.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
 
-            _buttonSetLabel = UIButton.FromType(UIButtonType.RoundedRect);
-            _label = new UILabel();
+            //_buttonSetLabel = UIButton.FromType(UIButtonType.RoundedRect);
+            //_label = new UILabel();
 
-            _buttonSetText = UIButton.FromType(UIButtonType.RoundedRect);
-            _textField = new UITextField();
-
+            //_buttonSetText = UIButton.FromType(UIButtonType.RoundedRect);
+            //_textField = new UITextField();
             _buttonSetLabel.Frame = new RectangleF(
-                View.Frame.Width / 2 - buttonWidth / 2,
+                View.Frame.Width / 2 - _buttonWidth / 2,
                 10,
-                buttonWidth,
-                buttonHeight);
+                _buttonWidth,
+                _buttonHeight);
             _label.Frame = new RectangleF(
-                View.Frame.Width / 2 - buttonWidth / 2,
+                View.Frame.Width / 2 - _buttonWidth / 2,
                 _buttonSetLabel.Frame.Y + _buttonSetLabel.Frame.Height,
-                buttonWidth,
-                buttonHeight);
+                _buttonWidth,
+                _buttonHeight);
 
             _buttonSetText.Frame = new RectangleF(
-                View.Frame.Width / 2 - buttonWidth / 2,
+                View.Frame.Width / 2 - _buttonWidth / 2,
                 _label.Frame.Y + _label.Frame.Height,
-                buttonWidth,
-                buttonHeight);
+                _buttonWidth,
+                _buttonHeight);
             _textField.Frame = new RectangleF(
-                View.Frame.Width / 2 - buttonWidth / 2,
+                View.Frame.Width / 2 - _buttonWidth / 2,
                 _buttonSetText.Frame.Y + _buttonSetText.Frame.Height,
-                buttonWidth,
-                buttonHeight);
+                _buttonWidth,
+                _buttonHeight);
             _textField.BorderStyle = UITextBorderStyle.RoundedRect;
 
             _buttonSetLabel.SetTitle("Set Label", UIControlState.Normal);
             _buttonSetText.SetTitle("Set Text", UIControlState.Normal);
 
-            ISimpleModel model = new SimpleModel();
-            _label.BindData(model, "LabelText");
-            _textField.BindData(model, "EditText");
+            //Model = new SimpleModel();
+            //_label.BindData(model, "LabelText");
+            //_textField.BindData(model, "EditText");
 
-            _buttonSetLabel.TouchUpInside += (object sender, EventArgs e) =>
+            _buttonSetLabel.TouchUpInside += (sender, e) =>
             {
-                Console.Out.WriteLine("SetLabelClick 1: Current Data Object Value: {0}", model.LabelText);
-                string text = string.Format("Label: Clicked {0} times.", numClicks++);
-                model.LabelText = text;
-                Console.Out.WriteLine("SetLabelClick : Current Data Object Value: {0}", model.LabelText);
+                Console.Out.WriteLine("==> SetLabelClick 1: Current Data Object Value: {0}", Model.LabelText);
+                //string text = string.Format("Label: Clicked {0} times.", _numberOfClicks++);
+                //Model.LabelText = text;
+                Presenter.UpdateLabelCount();
+                Console.Out.WriteLine("====> SetLabelClick : Current Data Object Value: {0}", Model.LabelText);
             };
-            _buttonSetText.TouchUpInside += (object sender, EventArgs e) =>
+            _buttonSetText.TouchUpInside += (sender, e) =>
             {
-                Console.Out.WriteLine("SetTextClick 1: Current Data Object Value: {0}", model.EditText);
-                string text = string.Format("Label: Clicked {0} times.", numClicks++);
-                model.EditText = text;
-                Console.Out.WriteLine("SetTextClick 1: Current Data Object Value: {0}", model.EditText);
-            };
+                Console.Out.WriteLine("==> SetTextClick 1: Current Data Object Value: {0}", Model.EditText);
+                //string text = string.Format("Label: Clicked {0} times.", _numberOfClicks++);
+                //Model.EditText = text;
+                Presenter.UpdateTextCount();
+                Console.Out.WriteLine("====> SetTextClick 1: Current Data Object Value: {0}", Model.EditText);
+            }; 
 
             _buttonSetLabel.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleTopMargin |
                 UIViewAutoresizing.FlexibleBottomMargin;
@@ -87,6 +100,15 @@ namespace Sample.UI.HelloWorld
             View.AddSubview(_textField);
         }
 
+
+
+        public void BindModel(ISimpleModel model)
+        {
+            Console.Out.WriteLine("BindModel(ISimpleModel)");
+            Model = model;
+            _label.BindData(Model, "LabelText");
+            _textField.BindData(Model, "EditText");
+        }
     }
 }
 
